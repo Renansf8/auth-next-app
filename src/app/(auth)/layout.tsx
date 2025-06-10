b5bd546d35112015769d2cf44645b31ac1462557
Renan Santiago <renansferreira8@gmail.com>
2025-06-10 08:28:17 -0300

    Add auth and dashboard redirect

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,16 @@
-export default function AuthLayout({
+import { getAccessToken } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const accessToken = await getAccessToken();
+
+  if (accessToken) {
+    redirect("/");
+  }
+
   return <div className="min-h-screen grid place-items-center">{children}</div>;
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,16 @@
-export default function DashboardLayout({
+import { getAccessToken } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const accessToken = await getAccessToken();
+
+  if (!accessToken) {
+    redirect("/sign-in");
+  }
+
   return <div className="min-h-screen grid place-items-center">{children}</div>;
 }
